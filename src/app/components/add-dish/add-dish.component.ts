f2b620746f687676b86d76119383f3c3c733e4e8
Nabil <najehnabil77@gmail.com>
2024-07-09 13:52:14 -1200

    add dish Form

--- src/app/components/add-dish/add-dish.component.ts
+++ src/app/components/add-dish/add-dish.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-dish',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-dish.component.css']
 })
 export class AddDishComponent implements OnInit {
+  addDishForm !: FormGroup
+  dish:any = {};
+  selectedChef !:""
+  chefs = [
+    { id: '1', firstName: 'John', lastName: 'Doe' },
+    { id: '2', firstName: 'Jane', lastName: 'Smith' },
+    { id: '3', firstName: 'Michael', lastName: 'Brown' }
+    // Ajoutez autant de chefs que nécessaire avec leurs données
+  ];
+
+  
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  addDish(): void {
+    // Vous pouvez implémenter ici la logique pour ajouter le plat
+    console.log('Plat à ajouter : ', this.dish);
+    console.log('Chef sélectionné : ', this.selectedChef);
+    // Exemple : appel à un service pour ajouter le plat avec le chef sélectionné
   }
 
 }
