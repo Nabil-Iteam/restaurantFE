52e5fde8c9306f9b44f7a31bdb82ad9e36aa08ed
Nabil <najehnabil77@gmail.com>
2024-07-12 00:34:37 -1200

    add BE

---
+++ restaurantBE/src/test/java/restaurant/restaurantBE/restaurantBE/RestaurantBeApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurant.restaurantBE.restaurantBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
